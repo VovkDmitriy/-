5f5d144d6fd6616214665041c114b31f2ebcaa18
VovkDmitriy <90185188+VovkDmitriy@users.noreply.github.com>
2021-11-22 15:31:05 +0200

    Add files via upload

---
+++ lab7.1/lab7.1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+namespace lab7._1
+{
+    class Program
+    {
+        private RSAParameters _publicKey;
+        private RSAParameters _privateKey;
+
+        public void AssignNewKey()
+        {
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+                _publicKey = rsa.ExportParameters(false);
+                _privateKey = rsa.ExportParameters(true);
+            }
+        }
+        public byte[] EncryptData(byte[] dataToEncrypt)
+        {
+            byte[] cipherbytes;
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                rsa.PersistKeyInCsp = false;
+                rsa.ImportParameters(_publicKey);
+                cipherbytes = rsa.Encrypt(dataToEncrypt, true);
+            }
+            return cipherbytes;
+        }public byte[] DecryptData(byte[] dataToEncrypt)
+        {
+            byte[] plain;
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                rsa.PersistKeyInCsp = false;
+                rsa.ImportParameters(_privateKey);
+                plain = rsa.Decrypt(dataToEncrypt, true);
+            }
+            return plain;
+        }
+        static void Main(string[] args)
+        {
+
+            var rsaParams = new Program();
+            const string original = "Dmytro Vovk";
+            rsaParams.AssignNewKey();
+            var encrypted = rsaParams.EncryptData(Encoding.UTF8.GetBytes(original));
+            var decrypted = rsaParams.DecryptData(encrypted);
+            Console.WriteLine("Text: " + original);
+            Console.WriteLine("Encrypted: " + Convert.ToBase64String(encrypted));
+            Console.WriteLine("Decrypted: " + Encoding.Default.GetString(decrypted));
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ lab7.2/lab7.2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace lab7._2
+{
+    class Program
+    {
+        public void AssignNewKey(string publicKeyPath = "public.xml", string privateKeyPath = "private.xml")
+        {
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+                File.WriteAllText(publicKeyPath, rsa.ToXmlString(false));
+                File.WriteAllText(privateKeyPath, rsa.ToXmlString(true));
+            }
+        }
+
+        public byte[] EncryptData(byte[] dataToEncrypt, string publicKeyPath = "public.xml")
+        {
+            byte[] cipherbytes;
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+                rsa.FromXmlString(File.ReadAllText(publicKeyPath));
+                cipherbytes = rsa.Encrypt(dataToEncrypt, false);
+            }
+            return cipherbytes;
+        }
+        public byte[] DecryptData(byte[] dataToEncrypt, string privateKeyPath = "private.xml")
+        {
+            byte[] plain;
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+                rsa.FromXmlString(File.ReadAllText(privateKeyPath));
+                plain = rsa.Decrypt(dataToEncrypt, false);
+            }
+            return plain;
+        }
+        static void Main(string[] args)
+        {
+            var rsaParams = new Program();
+            const string original = "Dmytro Vovk";
+            rsaParams.AssignNewKey();
+            var encrypted = rsaParams.EncryptData(Encoding.UTF8.GetBytes(original));
+            var decrypted = rsaParams.DecryptData(encrypted);
+            Console.WriteLine("Text: " + original);
+            Console.WriteLine("Encrypted: " + Convert.ToBase64String(encrypted));
+            Console.WriteLine("Decrypted: " + Encoding.Default.GetString(decrypted));
+            Console.ReadKey();
+        }
+    }
+}
